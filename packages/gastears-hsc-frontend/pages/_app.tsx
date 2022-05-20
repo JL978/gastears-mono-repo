8011922bb19f7b011f83aaa554e9aada569c55ff
Jimmy Lam <JimmyLam978@gmail.com>
2022-05-19 23:09:06 -0400

    uncomment some stuff

--- packages/gastears-hsc-frontend/pages/_app.tsx
+++ packages/gastears-hsc-frontend/pages/_app.tsx
@@ -7,15 +7,15 @@ import '../styles/globals.css'
 function MyApp({ Component, pageProps }: AppProps) {
   return (<>
     <Head>
-      {/* <title>Gas Tears - Cry About Your Gas Usage</title> */}
+      <title>GasTears X HSC</title>
       {/* <meta name="description" content="Keep track of all the gas that you spent through your blockchain exploration" /> */}
       {/* <meta name="image" property="og:image" content="https://live.staticflickr.com/65535/52074290310_e98ab166c1_k.jpg" /> */}
-      {/* <link rel="icon" href="/favicon.ico" /> */}
-      {/* <link rel="preconnect" href="https://fonts.googleapis.com" /> */}
-      {/* <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" /> */}
-      {/* <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" /> */}
-      {/* <link href="https://fonts.googleapis.com/css2?family=Inter&display=optional" rel="stylesheet" /> */}
-      {/* <script src="https://www.google.com/recaptcha/api.js?render=6LdYfEkeAAAAALIxY3AisT6fBgj12DW3aV8GDBWn"></script> */}
+      <link rel="icon" href="/favicon.ico" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+      <link href="https://fonts.googleapis.com/css2?family=Inter&display=optional" rel="stylesheet" />
+      <script src="https://www.google.com/recaptcha/api.js?render=6LdYfEkeAAAAALIxY3AisT6fBgj12DW3aV8GDBWn"></script>
     </Head>
     <WalletConnectProvider>
       <Component {...pageProps} />
